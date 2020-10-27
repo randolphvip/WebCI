922e448311eb2ed0665cc5cbc71ba3b26ecb6bc4
randolphvip <shangcongvip@163.com>
2020-10-27 21:41:33 +1300

    FristSubmit

---
+++ src/main/java/com/sc/webCI/servlet/FristServlet.java
@@ -0,0 +1,19 @@
+package com.sc.webCI.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FristServlet extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
